2804acfda3a45fee2f8e2493d970f1899ee9c8e8
Souza83 <tec.psouza@gmail.com>
2022-04-23 06:58:04 -0300

    Componente Item Refeição #01.

---
+++ lib/components/meal_item.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import '../models/meal.dart';
+
+class MealItem extends StatelessWidget {
+  final Meal meal;
+
+  const MealItem(this.meal);
+
+  void _selectMeal() {}
+
+  @override
+  Widget build(BuildContext context) {
+    // InkWell monitora evento de clique na tela
+    return InkWell(
+      onTap: _selectMeal,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        elevation: 4,
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            //Stack permite componente em cima do outro (texto + imagem etc...)
+            Stack(
+              children: [
+                // ClipRRect define as bordas p/ determinado componente
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.network(
+                    meal.imageUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
