0a2e6215fb986c58d49eb0dea762a450cba23b8b
Souza83 <tec.psouza@gmail.com>
2022-04-26 01:28:56 -0300

    Componente Item Refeição #02.

--- lib/components/meal_item.dart
+++ lib/components/meal_item.dart
@@ -37,7 +37,58 @@ class MealItem extends StatelessWidget {
                     fit: BoxFit.cover,
                   ),
                 ),
+                // Positioned possibilita fazer ancoramentos posicionais.
+                Positioned(
+                  right: 10, // Afasta da direita
+                  bottom: 20, // Afasta do fundo
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 20,
+                    ),
+                    child: Text(
+                      meal.title,
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true, // softWrap para qubera de linha
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
               ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround, // Espaçamento
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.schedule),
+                      SizedBox(width: 6),
+                      Text('${meal.duration} min'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.work),
+                      SizedBox(width: 6),
+                      Text(meal.complexityText),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.attach_money),
+                      SizedBox(width: 6),
+                      Text(meal.costText),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
--- lib/models/meal.dart
+++ lib/models/meal.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/foundation.dart'; // Referente ao required
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart'; // Referente ao required
 
 // Enumerações
 // Complexidade
@@ -44,4 +45,30 @@ class Meal {
     required this.complexity,
     required this.cost,
   });
+
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simples';
+      case Complexity.Medium:
+        return 'Normal';
+      case Complexity.Difficult:
+        return 'Difícil';
+      default:
+        return 'Desconhecida';
+    }
+  }
+
+  String get costText {
+    switch (cost) {
+      case Cost.Cheap:
+        return 'Barato';
+      case Cost.Fair:
+        return 'Justo';
+      case Cost.Expensive:
+        return 'Caro';
+      default:
+        return 'Desconhecido';
+    }
+  }
 }
