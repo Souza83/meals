130909cb09bf4740905f98319a47ff87eda00b3a
Souza83 <tec.psouza@gmail.com>
2022-04-27 05:20:18 -0300

    Adicionar TabBar na Parte Superior.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/tabs_screen.dart';
 import 'screens/categories_screen.dart';
 import 'screens/categories_meals_screen.dart';
 import 'screens/meal_detail_screen.dart';
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
       ),
       // Cria rotas entre telas
       routes: {
-        AppRoutes.HOME: (ctx) => CategoriesScreen(),
+        AppRoutes.HOME: (ctx) => TabsScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
         AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
       },
--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -5,23 +5,18 @@ import '../data/dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Vamos Cozinhar???'),
+    return GridView(
+      padding: const EdgeInsets.all(25), //Espaçamentos nas laterais das grids
+      //Renderiza as grids com scroll
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200, //Cada elemento com extenção de 200 px
+        childAspectRatio: 3 / 2, //Proporção de cada elemento (1,5 = 3 / 2)
+        crossAxisSpacing: 20, //Espaçamento no eixo cruzado
+        mainAxisSpacing: 20, //Espaçamento no eixo principal
       ),
-      body: GridView(
-        padding: const EdgeInsets.all(25), //Espaçamentos nas laterais das grids
-        //Renderiza as grids com scroll
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200, //Cada elemento com extenção de 200 px
-          childAspectRatio: 3 / 2, //Proporção de cada elemento (1,5 = 3 / 2)
-          crossAxisSpacing: 20, //Espaçamento no eixo cruzado
-          mainAxisSpacing: 20, //Espaçamento no eixo principal
-        ),
-        children: DUMMY_CATEGORIES.map((cat) {
-          return CategoryItem(cat);
-        }).toList(),
-      ),
+      children: DUMMY_CATEGORIES.map((cat) {
+        return CategoryItem(cat);
+      }).toList(),
     );
   }
 }
